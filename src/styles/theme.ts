1376b317912db44dd28c44ac17931a3aa2758d0c
Emanuel Filipe <emanuel8926@gmail.com>
2021-09-19 01:16:32 -0300

    style(theme): configure colors, spacings, sizes, and types

---
+++ src/styles/theme.ts
@@ -0,0 +1,69 @@
+export default {
+  grid: {
+    container: '108.5rem',
+    gutter: '1.9rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    normal: 400,
+    medium: 500,
+    bold: 700,
+    sizes: {
+      xsmall: '1.0rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.4rem',
+      xxxlarge: '3.8rem'
+    }
+  },
+  colors: {
+    mainBg: '#F1F1F1',
+    white: '#FCFCFC',
+    black: '#1D1D1D',
+    lightGray: '#F1F1F1',
+    gray: '#656565',
+    mediumGray: '#DEDEDE',
+    darkGray: '#2E2F42',
+    primary: '#204372',
+    mediumPrimary: '#778da3',
+    darkPrimary: '#001d46',
+    lightPrimary: '#526ea1',
+    secondary: '#3E98C7',
+    darkSecondary: '#006a96',
+    lightSecondary: '#77c9fa',
+    accent: '#F5A818',
+    darkAccent: '#bd7900',
+    lightAccent: '#ffd952',
+    background: '#F1F1F1',
+    secondaryBackground: '#DEDEDE',
+    surface: '#FCFCFC',
+    error: '#F54531',
+    success: '#36B369',
+    pending: '#C6F500',
+    textInBackground: '#656565',
+    textInSurface: '#1D1D1D'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+    xxxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
