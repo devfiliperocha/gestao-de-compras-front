5bb200c55b564719b39eaaa696ec282ab17d1734
Emanuel Filipe <emanuel8926@gmail.com>
2021-09-25 03:03:04 -0300

    style(progressbar): adjust background

--- src/components/Atoms/ProgressBar/styles.ts
+++ src/components/Atoms/ProgressBar/styles.ts
@@ -4,6 +4,6 @@ import ProgressBarBase from '@material-ui/core/LinearProgress'
 export const Wrapper = styled(ProgressBarBase)`
   ${({ theme }) => css`
     border-radius: ${theme.shape.borderRadius};
-    background: black;
+    background-color: ${theme.palette.background.secondary};
   `}
 `
