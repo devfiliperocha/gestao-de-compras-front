9e53eb0ea60044da312f8939cd6789ccf88e6eb2
Emanuel Filipe <emanuel8926@gmail.com>
2021-10-02 03:14:28 -0300

    feat(types): adjust

--- src/types/vendors.ts
+++ src/types/vendors.ts
@@ -1,10 +1,26 @@
+import { Address } from './address'
+import { File } from './file'
+
 export type Vendors = {
   corporateName: string
   fantasyName: string
+  corporateDocNumber: string
+  email?: string
+  phone?: string
+  responsible: string
+  responsibleDocNumber: string
+  address?: Address
   status: {
     type: 'error' | 'warning' | 'success'
     text: string
   }
+  corporateDocPdf?: File
+  federalCertificatePdf?: File
+  municipalCertificatePdf?: File
+  stateCertificatePdf?: File
+  laborCertificatePdf?: File
+  fgtsCertificatePdf?: File
+  declaration?: File
 }
 
 export type VendorColumns = keyof Vendors
